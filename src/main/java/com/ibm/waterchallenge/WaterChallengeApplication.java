f2ca9b01b03f1d79abf8baca23916ebd6b9c73b1
leslie burugu <leslieburugu@gmail.com>
2019-11-23 15:51:15 +0300

    Initial commit

---
+++ src/main/java/com/ibm/waterchallenge/WaterChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.waterchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaterChallengeApplication.class, args);
+    }
+
+}
