b07f653894da5e8c3299c9af03189a5e7dae77f6
leslie burugu <leslieburugu@gmail.com>
2019-11-28 21:23:56 +0300

    Remove basic-error-controller

--- src/main/java/com/ibm/waterchallenge/WaterChallengeApplication.java
+++ src/main/java/com/ibm/waterchallenge/WaterChallengeApplication.java
@@ -2,6 +2,12 @@ package com.ibm.waterchallenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -12,4 +18,12 @@ public class WaterChallengeApplication {
         SpringApplication.run(WaterChallengeApplication.class, args);
     }
 
+    @Bean
+    public Docket swaggerConfiguration() {
+        return new Docket(DocumentationType.SPRING_WEB)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
